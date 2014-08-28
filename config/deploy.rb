f347a5deda3bee56f4cda77354787dc6f6a38756
dmmoroka92 <dmoroka92@gmail.com>
2014-08-28 11:25:30 +0300

    deploying settings changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :deploy_via, :remote_cache
 
 # Default value for :linked_files is []
 
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{config/database.yml .env config/unicorn.rb}
 
 # Default value for linked_dirs is []
 
