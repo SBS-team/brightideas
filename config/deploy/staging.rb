03618794a401a16aaa90c030bf3a59f1532b4c3d
dmmoroka92 <dmoroka92@gmail.com>
2014-09-12 16:26:45 +0300

    comment_count field commented in migration

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
-set :branch, 'develop'
+set :branch, 'refact'
 
 # Simple Role Syntax
 # ==================
--- db/migrate/20140909140509_add_comments_count.rb
+++ db/migrate/20140909140509_add_comments_count.rb
@@ -2,9 +2,9 @@ class AddCommentsCount < ActiveRecord::Migration
   def change
     add_column :ideas, :comments_count, :integer, :default => 0
 
-    Idea.reset_column_information
-    Idea.all.each do |item|
-      item.update_attribute :comments_count, item.comments.length
+    #Idea.reset_column_information
+    #Idea.all.each do |item|
+    #  item.update_attribute :comments_count, item.comments.length
     end
   end
 end
