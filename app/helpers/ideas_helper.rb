cd62417e4d0f5dc3e568e685abb5eca399964315
SergeyKolenko <venty80@gmail.com>
2014-09-15 14:06:43 +0300

    Move images to assets

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,9 +1,9 @@
-module IdeasHelper #FIXME try to move in carrierwave
+module IdeasHelper
   def get_idea_avatar(idea)
     if !idea.avatar_id.nil?
       Attachment.find(idea.avatar_id).path.thumb_200x200.url
     else
-      '/img/no_image_available_thumb.jpg'
+      'no_image_available_thumb.jpg'
     end
   end
 
